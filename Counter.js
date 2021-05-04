343879e386c9d0627ff1d2e60dcd7a5e8c7c6c7c
CristianConcha <ciconcha@miuandes.cl>
2021-05-04 16:53:05 -0400

    final

--- Counter.js
+++ Counter.js
@@ -1,11 +1,29 @@
 
-const view = counter => counter;
+const view = counter => `   Counter: ${counter}\n\n     
+    (+) (-)\n\n
+    (q) for quit\n`;
 
 const update = (msg, counter) => {
     
-    if (msg === '+') return counter++;
-    else if (msg === '-') return counter--;
-    else return counter;
-        
+    if (msg === '+') return ++counter;
+    else if (msg === '-') return --counter;
+    else if (msg === 'q') return false;  
+    else return counter;  
 }
+const app = counter => {
 
+    while (true) {
+        
+        const currentView = view(counter);
+        console.clear();
+        console.log(currentView);
+
+        const prompt = require('prompt-sync')();
+        const msg = prompt('What would you do? ');
+        counter = update(msg, counter);
+
+        if (counter === false) break;
+
+    }
+}
+app(0);
